d6ffc4ec6a14b5a8956ea672b09cbeb95ffeb0d9
kuzminastya <57797237+kuzminastya@users.noreply.github.com>
2021-05-22 21:45:38 +0300

    Add files via upload

---
+++ Lab3_Kuzmina_Upgrade/Test.cs
@@ -0,0 +1,76 @@
+using System;
+using Xunit;
+using ViewModel_namespace;
+
+namespace Lab3_Kuzmina.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void Test_Init()
+        {
+            ViewModel VM = new ViewModel(new TestUIServices(true, true));
+            VM.AddDefaultV1DataOnGridCommand.Execute(null);
+            Assert.Equal(1, VM.MainCollection.Count);
+            VM.AddDefaultV1DataCollectionCommand.Execute(null);
+            Assert.Equal(2, VM.MainCollection.Count);
+            Assert.True(VM.MainCollection.ChangingAfterSave);
+        }
+        [Fact]
+        public void Test_FromFile()
+        {
+            ViewModel VM = new ViewModel(new TestUIServices(true, true));
+            VM.AddElementFromFileCommand.Execute(null);
+            Assert.Equal("data_from_file", VM.MainCollection.data[0].MeasurementInfo);
+            Assert.Equal(new DateTime(2020, 11, 18), VM.MainCollection.data[0].MeasurementDate);
+        }
+        [Fact]
+        public void Test_CustomData()
+        {
+            ViewModel VM = new ViewModel(new TestUIServices(true, true));
+            VM.enteredV1DataOnGrid.AddCustom();
+            Assert.Equal("Info is empty", VM.enteredV1DataOnGrid["Info"]);
+        }
+        [Fact]
+        public void Test_Open()
+        {
+            ViewModel VM = new ViewModel(new TestUIServices(false, true));
+            VM.MainCollection.AddDefaults();
+            Assert.True(VM.MainCollection.ChangingAfterSave);
+            VM.OpenCommand.Execute(null);
+            Assert.Equal(6, VM.MainCollection.Count);
+        }
+    }
+    public class TestUIServices: IUIServices
+    {
+        private readonly bool open;
+        private readonly bool save;
+        public TestUIServices(bool open, bool save)
+        {
+            this.open = open;
+            this.save = save;
+        }
+
+        public bool? ConfirmOpen(out string fileName)
+        {
+            fileName = "test_fromFile.txt";
+            return open;
+        }
+
+        public bool? ConfirmSave(out string fileName)
+        {
+            fileName = " ";
+            return save;
+        }
+
+        public void ShowMessage(string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Warning()
+        {
+            return false; // отказываемся сохранять несохраненные данные
+        }
+    }
+}
