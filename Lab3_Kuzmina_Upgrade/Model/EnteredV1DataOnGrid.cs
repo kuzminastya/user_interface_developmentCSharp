aa1224ce2417d7e7de92c790e087edbb2fb7c75b
kuzminastya <57797237+kuzminastya@users.noreply.github.com>
2021-05-22 21:44:50 +0300

    Add files via upload

---
+++ Lab3_Kuzmina_Upgrade/Model/EnteredV1DataOnGrid.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Lab3_V1_Kuzmina
+{
+    public class EnteredV1DataOnGrid : IDataErrorInfo, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private V1MainCollection collection;
+        private string info;
+        private int nodesNumber;
+        private float minValue;
+        private float maxValue;
+        public string Info
+        {
+            get { return info; }
+            set
+            {
+                info = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Info"));
+            }
+        }
+        public int NodesNumber
+        {
+            get { return nodesNumber; }
+            set
+            {
+                nodesNumber = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("NodesNumber"));
+            }
+        }
+        public float MinValue
+        {
+            get { return minValue; }
+            set
+            {
+                minValue = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MinValue"));
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MaxValue"));
+            }
+        }
+        public float MaxValue
+        {
+            get { return maxValue; }
+            set
+            {
+                maxValue = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MinValue"));
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MaxValue"));
+            }
+        }
+        public EnteredV1DataOnGrid() { }
+        public EnteredV1DataOnGrid(ref V1MainCollection _collection)
+        {
+            collection = _collection;
+        }
+        public void AddCustom()
+        {
+            float timeStep = 0.5f;
+            Grid grid = new Grid(0.0f, timeStep, NodesNumber);
+            V1DataOnGrid item = new V1DataOnGrid(Info, DateTime.Now, grid);
+            item.InitRandom(MinValue, MaxValue);
+            collection.Add(item);
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Info"));
+        }
+        public string Error { get { return "Error Text"; } }
+        public string this[string property]
+        {
+            get
+            {
+                string msg = null;
+                switch (property)
+                {
+                    case "Info":
+                        if (Info == null || Info.Length <= 0)
+                        {
+                            msg = "Info is empty";
+                        }
+                        foreach (V1Data item in collection.data)
+                        {
+                            if (item.MeasurementInfo == Info)
+                            {
+                                msg = "Info is empty";
+                            }
+                        }
+                        break;
+                    case "NodesNumber":
+                        if (NodesNumber <= 2) msg = "Not enough NodesNumber";
+                        break;
+                    case "MinValue":
+                    case "MaxValue":
+                        if (MinValue >= MaxValue) msg = "minValue is greater than maxValue";
+                        break;
+                }
+                return msg;
+            }
+        }
+    }
+}
---
+++ Lab3_Kuzmina_Upgrade/View/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows.Controls;
+using ViewModel_namespace;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel VM = new ViewModel(new WPFUIServices());
+        public static RoutedCommand AddCustomCommand = new RoutedCommand("AddCustom", typeof(View.MainWindow));
+        public void ChangeDataContext()
+        {
+            DataContext = VM.MainCollection;
+            commandsPanel.DataContext = VM;
+            commandsMenu1.DataContext = VM;
+            commandsMenu2.DataContext = VM;
+            commandsMenu3.DataContext = VM;
+            grid_AddCustom.DataContext = VM.enteredV1DataOnGrid;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangeDataContext();
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            VM.CheckingSave(new WPFUIServices());
+        }
+        private void MenuItem_New(object sender, RoutedEventArgs e)
+        {
+            VM.CheckingSave(new WPFUIServices());
+            VM = new ViewModel(new WPFUIServices());
+            ChangeDataContext();
+        }
+        private void CanRemoveCommandHandler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (listBox_Main.Items.Contains(listBox_Main.SelectedItem))
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+        }
+        private void RemoveCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            VM.MainCollection.RemoveAt(listBox_Main.SelectedIndex);
+        }
+        private void CanAddCustomCommandHandler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (grid_AddCustom != null)
+            {
+                foreach (FrameworkElement child in grid_AddCustom.Children)
+                {
+                    if (Validation.GetHasError(child) == true)
+                    {
+                        e.CanExecute = false;
+                        return;
+                    }
+                    e.CanExecute = true;
+                }
+            }
+            else e.CanExecute = false;
+        }
+        private void AddCustomCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            VM.enteredV1DataOnGrid.AddCustom();
+        }
+        private void FilterDataCollection(object sender, FilterEventArgs args)
+        {
+            VM.FilterDataCollection(ref args);
+        }
+        private void FilterDataOnGrid(object sender, FilterEventArgs args)
+        {
+            VM.FilterDataOnGrid(ref args);
+        }
+
+    }
+    public class WPFUIServices : IUIServices
+    {
+        public bool? ConfirmOpen(out string fileName)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            bool? result = dlg.ShowDialog();
+            fileName = dlg.FileName;
+            return result;
+        }
+        public void ShowMessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+        public bool Warning()
+        {
+            return MessageBox.Show("Предупреждение!\n " +
+                "Внесенные данные будут потеряны. Хотите ли Вы сохранить их?", "WARNING",
+                MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes;
+        }
+        public bool? ConfirmSave(out string fileName)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            bool? result = dlg.ShowDialog();
+            fileName = dlg.FileName;
+            return result;
+        }
+    }
+}
